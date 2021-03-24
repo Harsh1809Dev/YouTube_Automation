bab18f79120ff73f6071980ee766b69fe4fd5057
Harsh1809Dev <dev182000@gmai.com>
2021-03-24 21:22:53 +0530

    Incomplete

---
+++ script.js
@@ -0,0 +1,70 @@
+require("chromedriver");
+
+const wd = require("selenium-webdriver");
+const browser = new wd.Builder().forBrowser('chrome').build();
+
+async function main(){
+    await browser.get("https://youtube.com");
+    // await browser.wait(wd.until.elementLocated(wd.By.css("")));
+    // let sign_button = await browser.findElements(wd.By.css("role['button']"));
+    // console.log(sign_button);
+    // await sign_button.click();
+    // // console.log(sign_button.size);
+
+
+    
+
+ 
+
+    	//for searching
+  	await browser.wait(wd.until.elementLocated(wd.By.css("#search-icon-legacy")));
+    let find_button = await browser.findElement(wd.By.css("#search-icon-legacy"));
+	await browser.wait(wd.until.elementLocated(wd.By.css("input.ytd-searchbox")));
+	let search_button = await browser.findElement(wd.By.css("input.ytd-searchbox"));
+    // await browser.wait(wd.until.elementLocated(wd.By.xpath('//*[@id="search"]')));
+	// let search_button = await browser.findElement(wd.By.xpath('//*[@id="search"]'));
+	await search_button.click();
+	await search_button.sendKeys("pepcoding");
+	await find_button.click();
+
+    // 	//for playing first video
+	// await browser.wait(wd.until.elementLocated(wd.By.css("ytd-vertical-list-renderer.style-scope.ytd-vertical-list-renderer")));
+	// let videos = await browser.findElments(wd.By.css("ytd-vertical-list-renderer.style-scope.ytd-vertical-list-renderer"));
+	// console.log(videos.length);
+	// await videos[1].click();
+    // await browser.wait(wd.until.elementLocated(wd.By.xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]')));
+    // let first_video = await browser.findElement(wd.By.xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]'));
+    // await first_video.click();
+
+    //for signing
+    //await browser.wait(wd.until.elementLocated(wd.By.xpath('//*[@id="buttons"]/ytd-button-renderer/a')));
+    //let sign = await browser.findElement(wd.By.xpath('//*[@id="buttons"]/ytd-button-renderer/a'));
+    //await sign.click();
+    //for email
+    //await browser.wait(wd.until.elementLocated(wd.By.xpath('//*[@id="identifierId"]')));
+    //let email_id = await browser.findElement(wd.By.xpath('//*[@id="identifierId"]'));
+    //await email_id.click();
+    //await email_id.sendKeys("nibbaXnibbi6969@gmail.com");
+    //for next button
+    //await browser.wait(wd.until.elementLocated(wd.By.css(".VfPpkd-RLmnJb")));
+    //let next = await browser.findElement(wd.By.css(".VfPpkd-RLmnJb"));
+    //await next.click();
+    //for password
+    //await browser.wait(wd.until.elementLocated(wd.By.xpath('//*[@id ="password"]/div[1]/div / div[1]/input')));
+    //let password = await browser.findElement(wd.By.xpath('//*[@id ="password"]/div[1]/div / div[1]/input'));
+    //await password.sendKeys("N1bba69N1bb1");
+    //await browser.wait(wd.until.elementLocated(wd.By.xpath('//*[@id ="passwordNext"')));
+    //let next_password = await browser.findElement(wd.By.xpath('//*[@id ="passwordNext"'));
+    //await next_password.click();
+
+    // //for like
+    // await browser.wait(wd.until.elementLocated(wd.By.xpath('//*[@id="top-level-buttons"]/ytd-toggle-button-renderer[1]/a')));
+    // let like = await browser.findElement(wd.By.xpath('//*[@id="top-level-buttons"]/ytd-toggle-button-renderer[1]/a'));
+    // await like.click();
+
+    // //for subscribe
+    // await browser.wait(wd.until.elememtLocated(wd.By.xpath('//*[@id="button"]')));
+    // let susbcribe = await browser.findElement(wd.By.xpath('//*[@id="button"]'));
+    // await susbcribe.click();
+}
+main();
